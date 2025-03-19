da81201b1ceaad3490ef51672a3001b64f1662bf
adamsaleh11 <sadam1589@gmail.com>
2025-03-19 19:11:16 -0400

    Navbar complete

--- salehgroups/src/app/page.tsx
+++ salehgroups/src/app/page.tsx
@@ -1,103 +1,89 @@
+"use client";
 import Image from "next/image";
+import Link from "next/link";
+import { Link as ScrollLink, Element } from "react-scroll";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div
+      className="overflow-clip 
+    inset-0 
+    -z-10 h-full w-full bg-[#fafafa] 
+    bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] 
+    bg-[size:6rem_4rem]"
+    >
+      <Element
+        name="top"
+        className="overflow-hidden rounded-[6px] top-5 sticky md:mx-auto z-50 
+        xl:w-4/5 2xl:w-[68%] bg-white flex items-center 
+        justify-between py-6 px-4 md:px-8 mx-6"
+      >
+        <Link href={"/"}>
+          {/*}
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src={"/logo/logo.png"}
+          alt="logo"
+          width={1000}
+          height={1000}
+          className="w-28"
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+        */}
+        </Link>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+        {/* Navbar Links Full Width Fix */}
+        <div className="hidden md:flex flex-grow justify-center gap-x-10 items-center text-gray-700 font-medium text-lg cursor-pointer">
+          <Link href={"/showcase"} className="hover:text-blue-500">
+            Testimonials
+          </Link>
+          <ScrollLink
+            to="services"
+            smooth={true}
+            className="hover:text-blue-500"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            Services
+          </ScrollLink>
+          <ScrollLink
+            to="process"
+            smooth={true}
+            className="hover:text-blue-500"
           >
-            Read our docs
-          </a>
+            Process
+          </ScrollLink>
+          <ScrollLink
+            to="guarantees"
+            smooth={true}
+            className="hover:text-blue-500"
+          >
+            Guarantees
+          </ScrollLink>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="flex items-center gap-x-4">
+          <a href="tel:613-784-0342" className="hidden lg:flex text-blue-500">
+            <button className="px-4 py-2 rounded-md flex items-center gap-x-3">
+              613-784-0342
+            </button>
+          </a>
+          <Link
+            href={"/meeting"}
+            className="px-8 py-0.5  
+          border-2 border-black 
+          dark:border-white '
+          uppercase 
+          hover:bg-[#abcbff] 
+          text-black 
+          transition duration-200 
+          text-sm 
+          hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] "
+          >
+            Book a call
+          </Link>
+        </div>
+      </Element>
+      <div
+        className="absolute bottom-0 left-0 
+      right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"
+      ></div>
     </div>
   );
 }
