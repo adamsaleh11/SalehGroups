c4c933c66c55123358327c6982adb9e4c4871e4c
adamsaleh11 <sadam1589@gmail.com>
2025-06-14 00:18:33 -0400

    Update footer.tsx

--- salehgroups/src/components/ui/footer.tsx
+++ salehgroups/src/components/ui/footer.tsx
@@ -6,7 +6,7 @@ export default function footer() {
     <footer className="bg-[#fafafa] py-10  px-6 md:px-0 md:mx-auto border-t">
       <div className="flex flex-col  justify-between gap-y-3 xl:w-4/5 2xl:w-[68%] mx-auto">
         <h1 className="text-3xl md:text-5xl font-medium "></h1>
-        <p className="text-left  text-xl  text-gray-500">(613) 784 0343</p>
+        <p className="text-left  text-xl  text-gray-500">(613) 784 0342</p>
         <p className="text-left  text-xl  text-gray-500">
           adamsaleh0824@gmail.com
         </p>
